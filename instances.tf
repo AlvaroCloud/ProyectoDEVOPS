71dcb39d5b44c9e22c954a3fb09d64d31afb56fb
Alvaro <68744580+AlvaroCloud@users.noreply.github.com>
2020-07-24 17:10:54 +0200

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "nat" {
-  ami                         = "ami-0ba1c3ef9156479a2"
+  ami                         = "ami-004122be15033aa7ec"
   availability_zone           = "us-west-2a"
   instance_type               = "t2.micro"
   key_name                    = var.aws_key_name
